06c43ec2242a227728bff3dff9dea1e6df7ed02e
jcf <jcf@cwnu.com>
2023-04-28 12:36:51 +0800

    v1.0 git push02

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,5 +5,6 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("v1.0 master change");
         System.out.println("v1.0 git push");
+        System.out.println("v1.0 git push02");
     }
 }
